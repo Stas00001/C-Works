6357b01c2987e8a33837a5bef26d086d487a8500
Stas00001 <116278430+Stas00001@users.noreply.github.com>
2023-02-14 15:17:54 +0300

    fiet: добавил верстку

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+const mediaMobile = window.matchMedia('(max-width: 400px)');
+const btnFormText = document.querySelectorAll('.form__btn');
+const formFeedback = document.querySelectorAll('.form');
+const formInput = document.querySelectorAll('.form__input');
+if (mediaMobile.matches) {
+  btnFormText.forEach((btn, i) => {
+    btn.textContent = "✌️";
+  });
+  }
+  function handleFormSubmit(evt) {
+    evt.preventDefault();
+    formInput.forEach((item, i) => {
+      if (item.value !== '') {
+        btnFormText.forEach((btn, i) => {
+          btn.classList.add('animation');
+          if (mediaMobile.matches) {
+            btn.style.maxWidth = '30%';
+            btn.textContent = "Cпасибо!";
+          } else {
+            btn.textContent = "Круто, спасибо за доверие!";
+          }
+
+        });
+      }
+    });
+  }
+formFeedback.forEach((item, i) => {
+  item.addEventListener('submit', handleFormSubmit);
+});
+
+const links = document.querySelectorAll('.link[data-goto]');
+links.forEach((link) => {
+
+  link.addEventListener('click', onlinkClick);
+});
+function onlinkClick(e) {
+  const link = e.target;
+  if (link.dataset.goto && document.querySelector(link.dataset.goto)) {
+    const gotoBlock = document.querySelector(link.dataset.goto);
+    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth"
+    });
+    e.preventDefault();
+    }
+
+  }
